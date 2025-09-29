5795f95a42a338faa05c6ccac74463d632b795b4
ferLaika <fedesalvideal@outlook.com>
2025-09-29 15:20:42 -0500

    check route

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {signup, login, logout, updateProfile} from "../controllers/auth.controller.js";
+import {checkAuth, signup, login, logout, updateProfile} from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
@@ -12,7 +12,7 @@ router.post("/logout", logout)
 
 router.put("/update-profile", protectRoute, updateProfile)
 
-
+router.get("/check", protectRoute, checkAuth);
 
 
 
