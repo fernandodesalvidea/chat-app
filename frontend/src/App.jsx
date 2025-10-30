ee281155ca4da3f1fac1b861fe1e263d53a2e50f
ferLaika <fedesalvideal@outlook.com>
2025-10-30 14:41:19 -0500

    settings page themes working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,10 +11,12 @@ import { Routes, Route, Navigate} from 'react-router-dom';
 import { useAuthStore } from './store/useAuthStore';
 import {Loader} from "lucide-react";
 import { Toaster } from 'react-hot-toast';
+import { useThemeStore } from './store/useThemeStore';
 
 const App = () => {
 
   const {authUser, checkAuth, isCheckingAuth} = useAuthStore()
+  const { theme } = useThemeStore();
 
   useEffect(() => {
     checkAuth()
@@ -29,7 +31,7 @@ const App = () => {
   );
 
   return (
-  <div>
+  <div data-theme={theme}>
     <Navbar />
     <Routes>
         <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
