ebcf08f57346553ba046e530cdb9dd565d89fec6
ferLaika <fedesalvideal@outlook.com>
2025-11-10 16:55:26 -0600

    fix index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -32,11 +32,7 @@ if(process.env.NODE_ENV === "production"){
     app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     })
-} else {
-    app.get("/", (req, res) => {
-        res.send("API is running....")
-    })
-}
+} 
 
 
 server.listen(PORT, () => {
