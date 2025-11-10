44f510e06f4b7f2eb04f87d20da5d83fd9c17688
ferLaika <fedesalvideal@outlook.com>
2025-11-10 16:42:54 -0600

    project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 import { io, app, server } from "./lib/socket.js";
 
 
@@ -11,6 +12,7 @@ import {connectDB} from "./lib/db.js"
 dotenv.config()
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
@@ -23,6 +25,18 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    })
+} else {
+    app.get("/", (req, res) => {
+        res.send("API is running....")
+    })
+}
 
 
 server.listen(PORT, () => {
